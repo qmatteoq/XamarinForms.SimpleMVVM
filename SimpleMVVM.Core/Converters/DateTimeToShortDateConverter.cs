4a0829a30e69b6e7a0f17118cf634e2a672aa96c
qmatteoq <info@qmatteoq.com>
2015-01-05 13:20:10 +0100

    added missing files

---
+++ SimpleMVVM.Core/Converters/DateTimeToShortDateConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace SimpleMVVM.Core.Converters
+{
+    public class DateTimeToShortDateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                DateTime date = (DateTime) value;
+                return date.ToString("d");
+            }
+
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SimpleMVVM.Core/Localization/ILocalize.cs
@@ -0,0 +1,9 @@
+﻿using System.Globalization;
+
+namespace SimpleMVVM.Core.Localization
+{
+    public interface ILocalize
+    {
+        CultureInfo GetCurrentCultureInfo();
+    }
+}
