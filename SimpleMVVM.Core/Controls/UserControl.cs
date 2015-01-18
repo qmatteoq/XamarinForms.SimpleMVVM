a611e8536651f8d71342159fbb8fd9e399876c82
qmatteoq <info@qmatteoq.com>
2015-01-19 00:04:44 +0100

    Added UserControl support

---
+++ SimpleMVVM.Core/Controls/UserControl.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Xamarin.Forms;
+
+namespace SimpleMVVM.Core.Controls
+{
+    public class UserControl : Frame
+    {
+        private Dictionary<string, object> cache = new Dictionary<string, object>();
+
+        public UserControl()
+        {
+            this.Padding = new Thickness(0, 0, 0, 0);
+        }
+
+        protected T FindByViewPrivate<T>(string name)
+        {
+            if (this.cache.ContainsKey(name))
+            {
+                return (T)this.cache[name];
+            }
+
+            Type t = this.GetType();
+            FieldInfo fi = t.GetRuntimeFields().FirstOrDefault(f => f.Name == name);
+            if (fi == null) throw new NullReferenceException(string.Format("Field {0} not found.", name));
+            T value = (T)fi.GetValue(this);
+            this.cache.Add(name, value);
+            return value;
+        }
+    }
+}
